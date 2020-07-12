3d468f17a96a5e953d202b4a310305243b1a0443
Xanthanite <41843815+Xanthanite@users.noreply.github.com>
2020-07-12 15:36:14 -0400

    Major Script Update
    Added new functionalities along with the carousel, added animations, cleaned up some variables and added a small script to test the drop-downs for each project

--- pjava.js
+++ pjava.js
@@ -6,13 +6,23 @@ let introSub = document.getElementById("subTyped")
 
 let finalText = "Michael Martinez.";
 
-let subEnd = "I'm a web developer from New York that has a passion for creativity and efficiency. I have a strong focus in front-end development and I really enjoy the visual aspect of things but I also know my way around backend components."
+let subEnd = "I'm a web developer from New York that has a passion for creativity and efficiency. I have a strong focus in front-end development and I really enjoy the visual aspect of things but I also know my way around back-end components."
 
-var pages = ["pageOne", "pageTwo", "pageThree"]
+let pages = ["pageOne", "pageTwo", "pageThree"]
 
 let pageOn = document.getElementById("pageOn")
 
-let x = {   
+let sitePreview = document.getElementById("site");
+
+var idleListener;
+
+var currentIdle = ""
+
+var lastIdle = ""
+
+let nameLetters = 0
+
+let x = {//Sets x to be a variable with a consistent listener on it.
     pageselectInternal: "Introduction",
     
     pageselectListener: function(str) {},
@@ -31,15 +41,17 @@ let x = {
     }
 }
 
-x.registerListener(function(str) {
+x.registerListener(function(str) {//Takes x and registers the listener to then run functions
 
-    async function fader() {
+    async function fader() {//this async function ensures that it will check the page that it's on before fading out.
         let promise = new Promise((resolve, reject) => {
             pageOn.classList.add("fadeOut");
+            
             setTimeout (() => {
                 pageOn.innerHTML = str;
+
                 if (str == pageOn.innerHTML) {
-                    resolve("Done");
+                    resolve("Faded");
                 }
             }, 1300);
         });
@@ -54,38 +66,37 @@ x.registerListener(function(str) {
     fader()
 });
 
-function fadeIn(item) {
+function fadeIn(item) {//A simple fade in function
     item.classList.add("fadeIn");
 };
 
-setTimeout(function() {
+setTimeout(function() {//Fade in the introductory text
     fadeIn(header[0])
 }, 100)
 
-let e = 0
-
-function typer() {
-    if (e < finalText.length) {
-        introText.innerHTML += finalText.charAt(e);
-        e++;
+function typer() {//Typer for my name
+    if (nameLetters < finalText.length) {
+        introText.innerHTML += finalText.charAt(nameLetters);
+        nameLetters++;
         setTimeout(typer, 100);
-        if (e == finalText.length) {
+        
+        if (nameLetters == finalText.length) {
             introSub.innerHTML += subEnd;
             fadeIn(header[2]);
         }
     } 
 }
 
-setTimeout(typer, 1200);
+setTimeout(typer, 1200);//timeout on the typer (so that it works properly)
 
-function colorChanger(page, num) {
+function colorChanger(page) {//Take the page name passed in to clear that page from the menu bar
     document.getElementById(page).style.color = "transparent";
 
     document.getElementById(page).classList.add("unselectable");
 
     pages.forEach(function(ea) {
         if (ea !== page) {
-            document.getElementById(ea).style.color = "Maroon"
+            document.getElementById(ea).style.color = ""
             document.getElementById(ea).classList.remove("unselectable")
         }
     })
@@ -97,34 +108,81 @@ window.addEventListener("scroll", function() {
         var i = 0;
 
         if (window.scrollY < 347) { //if window.scrollY (Scroll position) is less than 347 (Desired page position) 
-            i = window.scrollY;//then set scrollY to i to freeze it
+            i = window.scrollY;//then set scrollY to i to freeze it in i
 
             if (i < 347) {//then we double check that i < 347 (to make sure it wasn't scrolled past immediately after)
                 currentPage = "Introduction"
                 x.pageselect = currentPage;
-                colorChanger("pageOne", 0);
+                colorChanger("pageOne");
             }
         }
         
-        if (window.scrollY > 347 && window.scrollY < 1241) {
-            i = window.scrollY
+        if (window.scrollY > 347 && window.scrollY < 1241) {//if window.scrollY is greater than 347 but less than 1241
+            i = window.scrollY//log the current scroll position in i then changes pageselect to the currentPage
             
             if (i < 1241) {
                 currentPage = "Skills"
                 x.pageselect = currentPage;
-                colorChanger("pageTwo", 1);
+                colorChanger("pageTwo");
                 console.log("yike");
             }
         }
 
-        if (window.scrollY > 1241) {
+        if (window.scrollY > 1241) {//and finally the last page
             i = window.scrollY
 
             if (i > 1241) {
                 currentPage = "Projects";
                 x.pageselect = currentPage
-                colorChanger("pageThree", 2);
+                colorChanger("pageThree");
             }
         }
     }, 500)
 });
+
+sitePreview.addEventListener("mousemove", function() {
+    
+    sitePreview.addEventListener("mouseout", function() {
+        clearTimeout(idleListener);
+        topOverlay.style.opacity = "0";
+        bottomOverlay.style.opacity = "0";
+    });
+
+    if (topOverlay.style.opacity !== "1" && bottomOverlay.style.opacity !=="1"); {
+        lastIdle = currentIdle;
+        clearTimeout(idleListener);
+        console.log("removed the overlay idle" + idleListener);
+        topOverlay.style.opacity = "1";
+        bottomOverlay.style.opacity = "1";
+        topOverlay.classList.toggle("top-overlay");
+        bottomOverlay.classList.toggle("bottom-overlay");
+        
+    };
+
+    idleListener = setTimeout(function() {
+        if(topOverlay.style.opacity !== "0" && bottomOverlay.style.opacity !=="0"){
+            currentIdle = idleListener;
+            // console.log("added the idle class");
+            topOverlay.style.opacity = "0";
+            bottomOverlay.style.opacity = "0";
+            topOverlay.classList.toggle("top-overlay");
+            bottomOverlay.classList.toggle("bottom-overlay");
+        }
+    }, 1500);
+});
+
+var infoStatus = "closed"
+
+$(".moreInfo").click(function() {
+    if (infoStatus == "closed") {
+        $("#siteDesc1").slideDown();
+        moreInfo1.style.transform = "rotate(180deg)";
+        return(infoStatus = "open");
+    };
+
+    if (infoStatus == "open") {
+        $("#siteDesc1").slideUp();
+        moreInfo1.style.transform = "";
+        return(infoStatus = "closed");
+    }
+})
